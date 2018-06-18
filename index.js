79a786fdc7139b48dbe62b9b77ca3fb86d91952c
Thanakorn Piroonsith <admin@extentsoft.com>
2018-06-19 00:50:44 +0700

    Test Pull Request

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 const logger = require('winston')
 const processType = process.env.PROCESS_TYPE
 logger.info(`Attempting to start ${processType} process`, { pid: process.pid })
+
+console.log('test pull request');
